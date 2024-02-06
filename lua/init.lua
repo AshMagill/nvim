e35c35d385e70c10946eb5e18c842a9778aa0bbd
ashmagill <ashmagill@developersinstitute.co.nz>
2024-02-07 01:38:11 +1300

    logs list works and changed keys

--- lua/init.lua
+++ lua/init.lua
@@ -10,24 +10,30 @@ require('local-ai')
 require('chat-gpt')
 --require('hardtime').setup()
 
+   local log_filter = require('log_filter')
+   -- Add a Neovim command that calls the create_log function
+   vim.api.nvim_create_user_command('GetLogs', log_filter.getLogs, {})
+   -- You can also set up a keybinding if you prefer
+   vim.api.nvim_set_keymap('n', '<leader>cll', ':GetLogs<CR>', { noremap = true, silent = true })
+
    local log_creator = require('log_creator')
    -- Add a Neovim command that calls the create_log function
    vim.api.nvim_create_user_command('CreateLog', log_creator.create_log, {})
    -- You can also set up a keybinding if you prefer
-   vim.api.nvim_set_keymap('n', '<leader>ll', ':CreateLog<CR>', { noremap = true, silent = true })
+   vim.api.nvim_set_keymap('n', '<leader>cln', ':CreateLog<CR>', { noremap = true, silent = true })
 
    local folder_creator = require('folder_creator')
    -- Add a Neovim command that calls the create_folder function
    vim.api.nvim_create_user_command('CreateFolder', folder_creator.create_folder, {})
    -- You can also set up a keybinding if you prefer
-   vim.api.nvim_set_keymap('n', '<leader>lc', ':CreateFolder<CR>', { noremap = true, silent = true })
+   vim.api.nvim_set_keymap('n', '<leader>eln', ':CreateFolder<CR>', { noremap = true, silent = true })
 
    local todo_list = require('todo_list')
 vim.api.nvim_create_user_command('ListTodos', todo_list.list_todo, {})
-vim.api.nvim_set_keymap('n', '<leader>tdl', ':ListTodos<CR>', {noremap= true})
+vim.api.nvim_set_keymap('n', '<leader>dll', ':ListTodos<CR>', {noremap= true})
 
    local todo_create = require('todo_create')
 vim.api.nvim_create_user_command('CreateTodos', todo_create.create_todo, {})
-vim.api.nvim_set_keymap('n', '<leader>tdn', ':CreateTodos<CR>', {noremap= true})
+vim.api.nvim_set_keymap('n', '<leader>dln', ':CreateTodos<CR>', {noremap= true})
 
 
--- lua/log_filter.lua
+++ lua/log_filter.lua
@@ -1,110 +1,102 @@
 local M = {}
--- Function to read a CSV file and return its contents as a table
-local function read_csv(file_path)
-    local file = io.open(file_path, "r")
-    local lines = {}
-    local isFirstLine = true
-    for line in file:lines() do
-        if not isFirstLine then
-            local fields = {}
-            for value in line:gmatch("[^,]+") do
-                table.insert(fields, value)
-            end
-            table.insert(lines, fields)
-        else
-            isFirstLine = false
-        end
-    end
-    file:close()
-    return lines
-end
--- Function to prompt the user for sorting options
-local function prompt_sort_options()
-    local options = {
-        "Sort by Date Created",
-        "Sort by Urgency"
-    }
-    -- Create a numbered list of sorting options
-    local numbered_options = {}
-    for i, option in ipairs(options) do
-        table.insert(numbered_options, i .. ". " .. option)
-    end    
-    local choice = vim.fn.inputlist(numbered_options)
-    
-    -- Restoring the original choice index
-    choice = tonumber(choice)
-return choice
-end
--- Function to handle the workflow
-function M.list_todo()
-    -- Specify the absolute path of the Workflow folder
-        local path_choice = vim.fn.input("Choose path (1. Workspace, 2. Workflow): ")
-    local workflow_path
-    if path_choice == "1" then
-        workflow_path = "/home/ash/Workspace"
+function M.getLogs()
+        local logsPath
+    local logsOption = vim.fn.inputlist({"1. Workflow", "2. Workspace"})
+    if logsOption == 1 then
+        logsPath = "/home/ash/Documents/Workflow"
+    elseif logsOption == 2 then
+        logsPath = "/home/ash/Workspace"
     else
-        workflow_path = "/home/ash/Documents/Workflow"
-    end    
-    vim.api.nvim_out_write("\n")
-    vim.api.nvim_out_write("\n")
-    
-    -- Get a list of all subfolders in the Workflow folder
-    local subfolders = vim.fn.globpath(workflow_path, "*", 1, 1)
-    
-    -- Prompt the user to select a subfolder or select all subfolders
-    local numbered_options = {}
-    for i, subfolder in ipairs(subfolders) do
-        table.insert(numbered_options, i .. ". " .. subfolder)
+        print("Invalid option.")
+        return nil
     end
-    table.insert(numbered_options, "All")
-    
-    local choice = vim.fn.inputlist(numbered_options)
-    vim.api.nvim_out_write("\n")
-    
-    -- Create a buffer to display the todos
-    vim.cmd("enew")
-    local bufnr = vim.api.nvim_get_current_buf()
-    
-    -- Read the todo.csv file(s) based on the user's choice
-    local todo_lines = {}
-    if choice == #subfolders + 1 then
-        -- Concatenate all todo.csv files into one buffer
-        for _, subfolder in ipairs(subfolders) do
-            local todo_file = subfolder .. "/Todo/todo.csv"
-            local lines = read_csv(todo_file)
-            for _, line in ipairs(lines) do
-                table.insert(todo_lines, line)
+
+        vim.api.nvim_out_write("\n")
+
+    local logsBuffer = vim.api.nvim_create_buf(false, true)
+    -- Concatenate logs from a specific folder into the buffer
+    local function concatLogs(folderPath)
+        local logFiles = vim.fn.readdir(folderPath)
+        local sortedFiles = {}
+        for _, file in ipairs(logFiles) do
+            local filePath = folderPath .. "/" .. file
+            if vim.fn.isdirectory(filePath) == 0 then
+                table.insert(sortedFiles, {file = file, path = filePath, mtime = vim.fn.getftime(filePath)})
             end
         end
-else
-        -- Read the selected subfolder's todo.csv file
-        local todo_file = subfolders[choice] .. "/Todo/todo.csv"
-        todo_lines = read_csv(todo_file)
-    end    
-    -- Sort the todo lines based on the user's choice
-    local sort_choice = prompt_sort_options()
-    if sort_choice == 1 then
-        table.sort(todo_lines, function(a, b)
-            return a[3] > b[3]
+        table.sort(sortedFiles, function(a, b)
+            return a.mtime < b.mtime
         end)
-    elseif sort_choice == 2 then
-        table.sort(todo_lines, function(a, b)
-            return tonumber(a[4]) < tonumber(b[4])
+        for _, fileInfo in ipairs(sortedFiles) do
+            local header = vim.fn.fnamemodify(fileInfo.file, ":t:r")
+            --header = header .. " - " .. vim.fn.fnamemodify(vim.fn.fnamemodify(folderPath, ":h"), ":t")
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {""})
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {header})
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {""})
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, vim.fn.readfile(fileInfo.path))
+        end
+    end
+    local function traverseDirectory(directory)
+        local subfolders = vim.fn.readdir(directory)
+        local sortedSubfolders = {}
+        for _, subfolder in ipairs(subfolders) do
+            local subfolderPath = directory .. "/" .. subfolder
+            if vim.fn.isdirectory(subfolderPath) == 1 then
+                local logsFolderPath = subfolderPath .. "/Logs"
+                if vim.fn.isdirectory(logsFolderPath) == 1 then
+                    table.insert(sortedSubfolders, {path = logsFolderPath, mtime = vim.fn.getftime(logsFolderPath), folder = subfolder})
+                end
+            end
+        end
+        table.sort(sortedSubfolders, function(a, b)
+            return a.mtime > b.mtime
         end)
-    end    
-    -- Clear the buffer and display the sorted todo lines
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, {})
-    for _, line in ipairs(todo_lines) do
-        vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, line)
-        vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, {""}) -- Add an empty line after each line
-    end    
-    -- Set the buffer options
-    vim.api.nvim_buf_set_option(bufnr, "modifiable", false)
-    vim.api.nvim_buf_set_option(bufnr, "buftype", "nofile")
-    vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
-    vim.api.nvim_buf_set_option(bufnr, "filetype", "todo")
-    -- Switch to the todo buffer
-    vim.cmd("buffer " .. bufnr)
+        return sortedSubfolders
+    end
+    -- Prompt for folder selection
+    local folders = traverseDirectory(logsPath)
+    local selectedFolders = {}
+    local allOption = "1. All"
+    table.insert(selectedFolders, allOption)
+    for i, folderInfo in ipairs(folders) do
+        table.insert(selectedFolders, i +1 .. ". " .. folderInfo.folder)
+    end
+    local selectedFolderIndex = vim.fn.inputlist(selectedFolders)
+    if selectedFolderIndex <= 0 or selectedFolderIndex > #selectedFolders then
+        return nil
+    end
+    -- Concatenate logs based on folder selection
+    if selectedFolderIndex == 1 then
+        -- Concatenate logs for all folders
+        for i, folderInfo in ipairs(folders) do
+            local folderHeader = "=== " .. i .. ". " .. folderInfo.folder .. " ==="
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {""})
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {folderHeader})
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {""})
+            concatLogs(folderInfo.path)
+        end
+    else
+        -- Concatenate logs for the selected folder index
+        local selectedFolderPath = logsPath .. "/" .. folders[selectedFolderIndex - 1].folder .. "/Logs"
+        if vim.fn.isdirectory(selectedFolderPath) == 1 then
+            local folderHeader = "=== " .. selectedFolderIndex .. ". " .. folders[selectedFolderIndex - 1].folder .. " ==="
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {""})
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {folderHeader})
+            vim.api.nvim_buf_set_lines(logsBuffer, -1, -1, false, {""})
+            concatLogs(selectedFolderPath)
+        else
+            print("Folder not found.")
+            return nil
+        end
+    end
+    -- Open the logs buffer and jump to the bottom
+    vim.api.nvim_command("split")
+    vim.api.nvim_command("buffer " .. logsBuffer)
+    vim.api.nvim_command("normal! G")
+    -- Set buffer options
+    vim.api.nvim_buf_set_option(logsBuffer, "modifiable", false)
+    vim.api.nvim_buf_set_option(logsBuffer, "buftype", "nofile")
+    vim.api.nvim_buf_set_option(logsBuffer, "bufhidden", "wipe")
+    return logsBuffer
 end
 return M
-
